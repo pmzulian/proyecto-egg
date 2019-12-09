28b458a39c2f372ca2228ecc553b197bc5cfdbe4
enzoprieto <58447086+enzoprieto@users.noreply.github.com>
2019-12-09 16:48:50 -0300

    Add files via upload

---
+++ tatoo/src/main/java/edu/egg/tatoo/TatooApplication.java
@@ -0,0 +1,13 @@
+package edu.egg.tatoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatooApplication.class, args);
+	}
+
+}
