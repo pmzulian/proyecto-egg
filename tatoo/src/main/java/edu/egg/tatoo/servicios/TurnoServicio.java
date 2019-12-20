043203b9eee222ef0271de8dc9993e4293d05e73
enzoprieto <35614931>
2019-12-20 11:07:14 -0300

    algo de front~!

--- tatoo/src/main/java/edu/egg/tatoo/servicios/TurnoServicio.java
+++ tatoo/src/main/java/edu/egg/tatoo/servicios/TurnoServicio.java
@@ -1,7 +1,11 @@
 
 package edu.egg.tatoo.servicios;
 
+import edu.egg.tatoo.entidades.Proveedor;
+import edu.egg.tatoo.entidades.Turno;
+import edu.egg.tatoo.entidades.Usuario;
 import edu.egg.tatoo.repositorios.TurnoRepositorio;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +13,17 @@ import org.springframework.stereotype.Service;
 public class TurnoServicio {
     @Autowired
     private TurnoRepositorio turnorepositorio;
+    
+    public Turno guardarTurno  (Proveedor proveedor, Usuario usuario, Date fecha){
+        Turno turno = new Turno ();
+        turno.setFecha(fecha);
+        turno.setProveedor(proveedor);
+        turno.setUsuario(usuario);
+        
+        return turno;
+
+}
+    
+    
     
 }
